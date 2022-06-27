8285ba804fa20be28459899c977afb926a352872
Furkan394 <furkanncakiroglu@gmail.com>
2022-06-27 22:52:25 +0300

    added results tools to provide notification on data

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added successfully";
+        public static string ProductNameInvalid = "The product name must be at least two characters.";
+        public static string ProductListed = "Products listed";
+        public static string MaintenanceTime = "Maintenance time...";
+    }
+}
