c74fca078582bdfed7b74cbf5adba5a304722035
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-05 17:19:05 +0300

    business rules added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "The product name must be at least two characters.";
         public static string ProductListed = "Products listed";
         public static string MaintenanceTime = "Maintenance time...";
+        public static string ProductCountOfCategoryError = "A category can have up to ten products";
+        public static string ProductNameAlreadyExists = "Product name already exists";
+        public static string CategoryLimitExceded = "Category limit exceded";
     }
 }
